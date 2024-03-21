52fda1e2fa83a0f1b59843eacdc9c9a8e70aa9e3
Gatmin50 <tonilube6@gmail.com>
2024-03-21 20:52:27 +0100

    a

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ import os
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_mapping(
+        SENDGRID_KEY=os.environ.get('SENDGRID_KEY'),
+    )
+
     from . import portfolio
 
     app.register_blueprint(portfolio.bp)
